100668249a2ac904fe1d69a8ab7e69a54ab6ff4a
cad56 <69633639+cad56@users.noreply.github.com>
2021-04-09 20:02:25 +0530

    Update Q5.cpp

--- Q5/Q5.cpp
+++ Q5/Q5.cpp
@@ -1,64 +1,52 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-//  creating structure for node
 struct node {
 	int d;
 	node* nxt;
 };
 
 
-// The data and the head of linked list
-//is taken as an arguement by the
-//function and if linkedlist  exists,
-//  already, it appends given node at 
-//end of the last node if linkedlist 
-//doesn't exists, it creates one with the
-// head pointing to first node.
-node* dataget(node* head, int num)
+node* insertval(node* hoflist, int numb)//We got the head of the list and number to be inserted 
 {
+    //Basic idea is if LL exists already we append the presend node at the last node
+    //if LL doesn't exist we create one head pointing to first node 
+    
+	node* t = new node;//Craeting a temp node t
+	
+	node* toflist = hoflist;//Creating a node tail of list same as head of list
 
-	node* t = new node;
-	node* tail = head;
-
-	// data is inserted into the temporary
-	// node and point it's next to NULL
-	t->d = num;
+	
+	t->d = numb;//Now t contains the value to be inserted and the next pointer which is obviously null 
 	t->nxt = NULL;
 
-	// If head is null, create a
-	// linked list with t as head
-	// and tail of the list
-	if (head == NULL) {
-		head = t;
-		tail = t;
+	
+	if (hoflist == NULL) {//If the given list was empty we will assign the node to it 
+		hoflist = t;
+		toflist = t;
 	}
 
-	// Else inserting the temporary node
-	// after the tail of the existing
-	// node and make the temporary node
-	// as the tail of the linked list
 	else {
 
-		while (tail != NULL) {
+		while (toflist != NULL) {//If it is noth empty then  untill we find a last node we iterate and once 
+		//we find the last node we just add our current to that last node
 
-			if (tail->nxt == NULL) {
-				tail->nxt = t;
-				tail = tail->nxt;
+			if (toflist->nxt == NULL) {
+				toflist->nxt = t;
+				toflist = toflist->nxt;
 			}
-			tail = tail->nxt;
+			toflist = toflist->nxt;
 		}
 	}
 
-	return head;
+	return hoflist;//Returning the modified list 
 }
 
-// Function is to print the linkedlist
-void dataset(node* head)
+
+void printdata(node* hoflist)
 {
 	node* temp;
-	temp = head;
+	temp = hoflist;
 
 	while (temp != NULL) {
 
@@ -70,40 +58,39 @@ void dataset(node* head)
 }
 
 
-// Function is to concatenate the two lists
-node* listmerge(node** h1,
-				node** h2)
+
+node* listmerge(node** h1,node** h2)
 {
+    //so here we are basically taking both the lists and appending them one after the other 
+	node* toflist = *h1;//Firstly we point to first list 
+	while (toflist != NULL) {
 
-	node* tail = *h1;
-	while (tail != NULL) {
-
-		if (tail->nxt == NULL
-			&& h2 != NULL) {
-			tail->nxt = *h2;
+		if (toflist->nxt == NULL
+			&& h2 != NULL) {  //When we reach the end of it and we have anyother list remaining which is not null
+			toflist->nxt = *h2;//Just start pointing to head of that list and do the same
 			break;
 		}
-		tail = tail->nxt;
+		toflist = toflist->nxt;
 	}
 
-	// return the concatenated lists as a
-	// single list - h1
-	return *h1;
+	
+	return *h1;  //at last return the lead of the list where the modified list is stored it is h1
 }
 
-// Sorting the linkedlist using bubble sort
+
 void listsort(node** h1)
 {
 	node* curr = *h1;
 	node* t= *h1;
 
-	
+	//Basic idea of selection sort we are using here we implemend the idea of mergge sort well as well in
+	//the last question please check that as well 
 	while (curr->nxt != NULL) {
 
 		t = curr->nxt;
-		while (t != NULL) {
-
-			if (t->d< curr->d) {
+		while (t != NULL) {//Basically swapping 2 nodes curr and the other node next to it if 
+            //they violate the sorting condition basicall is i < j then a[i] <= a[j] is its a[i] > a[j] we are swapping 
+ 			if (t->d< curr->d) {
 				int temp = t->d;
 				t->d = curr->d;
 				curr->d = temp;
@@ -117,31 +104,24 @@ void listsort(node** h1)
 
 int main()
 {
-	node* h1 = new node;
+	node* h1 = new node;//Declaring the lists 
 	node* h2 = new node;
 
 	h1 = NULL;
 	h2 = NULL;
 
-	// Taken Linked List 1
-	h1 = dataget(h1, 5);
-	h1 = dataget(h1, 8);
-	h1 = dataget(h1, 6);
 
-	// Taken Linked List 2
-	h2 = dataget(h2, 3);
-	h2 = dataget(h2, 2);
-	h2 = dataget(h2, 9);
-	h2 = dataget(h2, 2);
+    //Inserting the corresponding values into the lists 
+	h1 = insertval(h1, 4);
+	h1 = insertval(h1, 2);
+	h1 = insertval(h1, 7);
+	h2 = insertval(h2, 5);
+	h2 = insertval(h2, 1);
 
-	// Merge the two lists
-	// in a single list
-	h1 = listmerge(&h1,&h2);
+	h1 = listmerge(&h1,&h2);//Merging both the lists 
 
-	// Sort the unsorted merged list
-	listsort(&h1);
+	listsort(&h1);//Sorting the merged list 
 
-	// sorted merged list
-	dataset(h1);
+	printdata(h1);//Finally print ing the list 
 	return 0;
 }
